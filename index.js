b8efb125c7cb040fd8838c0b00978e45e6e6a6ca
Milan Dhar <milan.dhar@gmail.com>
2019-02-13 20:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  return object[key]
+  delete object[key]
+  return object
 }
 
